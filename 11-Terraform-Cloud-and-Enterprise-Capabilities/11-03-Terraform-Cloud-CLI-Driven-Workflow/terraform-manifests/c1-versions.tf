4985fa7f7e43105567712cf84f0879a8cf082d0f
FOdeks <99102643+FOdeks@users.noreply.github.com>
2023-08-13 20:43:15 -0400

    Update c1-versions.tf

--- 11-Terraform-Cloud-and-Enterprise-Capabilities/11-03-Terraform-Cloud-CLI-Driven-Workflow/terraform-manifests/c1-versions.tf
+++ 11-Terraform-Cloud-and-Enterprise-Capabilities/11-03-Terraform-Cloud-CLI-Driven-Workflow/terraform-manifests/c1-versions.tf
@@ -7,7 +7,7 @@ terraform {
       version = "~> 5.0"
     }
   }
-  # Update Terraform Cloud Backend Block Information below
+  # Update Terraform Cloud Backend Block Information belo
   backend "remote" {
     organization = "hcta-demo1"
 
